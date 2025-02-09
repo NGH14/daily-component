2a4b31e9f2600bb29c00dc0d1e10252a2dc77132
nghia <naovu@protonmail.com>
2025-02-09 22:58:45 +0700

    feat: increase typing speed for improved user experience

--- 029.Simple Text Typing/script.js
+++ 029.Simple Text Typing/script.js
@@ -1,7 +1,7 @@
 let i = 0;
 let typing = document.getElementById('typing__text');
 const STRING = 'Simple Typing with setTimeout';
-const SPEED = 1;
+const SPEED = 50;
 
 if (i < STRING.length) {
     typing.textContent += STRING.charAt(i);
