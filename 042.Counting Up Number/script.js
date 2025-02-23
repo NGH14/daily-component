5a4548b2d438a5f589c9d5f86c76bdbd267d54c4
nghia <naovu@protonmail.com>
2025-02-23 13:06:45 +0700

    feat: add Day #42 example of Counting Up Number animation with HTML, CSS, and JavaScript

---
+++ 042.Counting Up Number/script.js
@@ -0,0 +1,27 @@
+function countingUp(element, endValue = 100, duration = 1000) {
+  let startTime = null;
+  let currentNumber = 0;
+  function animate(currentTime) {
+    if (!startTime) {
+      startTime = currentTime;
+    }
+
+    let progress = (currentTime - startTime) / duration;
+
+    progress = Math.min(progress, 1);
+
+    currentNumber = Math.floor(progress * endValue);
+
+    element.textContent = currentNumber;
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
+
+countingUp(document.getElementById('counting__number'), 500,3000);
+
+
