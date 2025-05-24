5b0b20c2b969b0bad835d1c01bd628272ec19ded
nghia <naovu@protonmail.com>
2025-05-24 23:02:14 +0700

    chore(#8): remove comment

--- 008.Simple Grids/script.js
+++ 008.Simple Grids/script.js
@@ -1,6 +1,3 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
-
 const collections = () => {
   return fetch("data.json")
     .then((response) => response.json())
