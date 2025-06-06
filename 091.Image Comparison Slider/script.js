0c140aacd45a991ee78344c3fb251ab74445f9b2
nghia <naovu@protonmail.com>
2025-06-07 00:59:08 +0700

    refactor(#91): rename slider class to slider-input for consistency

--- 091.Image Comparison Slider/script.js
+++ 091.Image Comparison Slider/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.slider-container');
-  document.querySelector('.slider').addEventListener('input', (e) => {
+  document.querySelector('.slider-input').addEventListener('input', (e) => {
     container.style.setProperty('--indicator', `${e.target.value}%`);
   });
 });
