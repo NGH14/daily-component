5817ed7dca7657704258863192f9d4d6646f8ce4
nghia <naovu@protonmail.com>
2025-06-09 22:50:20 +0700

    chores(#91): recorrect the name of compare variable

--- 091a.Horizontal Image Comparison Slider/script.js
+++ 091a.Horizontal Image Comparison Slider/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.slider-container');
   document.querySelector('.slider-input').addEventListener('input', (e) => {
-    container.style.setProperty('--indicator', `${e.target.value}%`);
+    container.style.setProperty('--position', `${e.target.value}%`);
   });
 });
