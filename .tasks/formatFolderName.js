b87e231895b574e40f95523fbdf531ca35ea90ff
Nghia <naovu@protonmail.com>
2025-07-16 23:41:39 +0700

    chore(tanks): remove whitespace and meanless comment

--- .tasks/formatFolderName.js
+++ .tasks/formatFolderName.js
@@ -1,9 +1,6 @@
 
 export function formatWord(word) {
-  // Keep all-uppercase words like CSS, API
   if (word === word.toUpperCase()) return word;
-
-  // Handle hyphenated words like focus-within
   return word
     .split('-')
     .map(part =>
@@ -16,19 +13,14 @@ export function formatFolderName(namePart) {
   return namePart
     .split(' ')
     .map(word => {
-      // If word starts and ends with parentheses
       if (word.startsWith('(') && word.endsWith(')')) {
         const inner = word.slice(1, -1);
         return '(' + formatWord(inner) + ')';
       }
-
-      // If only starts with '('
       if (word.startsWith('(')) {
         const inner = word.slice(1);
         return '(' + formatWord(inner);
       }
-
-      // If only ends with ')'
       if (word.endsWith(')')) {
         const inner = word.slice(0, -1);
         return formatWord(inner) + ')';
