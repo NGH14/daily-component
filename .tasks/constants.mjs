b1028637de38378dde07a031c01c557629dceb15
Nghia <naovu@protonmail.com>
2025-08-15 23:51:02 +0700

    chore: export default table marker

--- .tasks/constants.mjs
+++ .tasks/constants.mjs
@@ -1,7 +1,8 @@
 const startMarker = '<!-- PROGRESS TABLE START -->';
 const endMarker = '<!-- PROGRESS TABLE END -->';
 
-export const tableMDRegex = new RegExp(`(${startMarker}\\n)([\\s\\S]*?)\\n(${endMarker})`);
+const tableMDRegex = new RegExp(`(${startMarker}\\n)([\\s\\S]*?)\\n(${endMarker})`);
 
+export default tableMDRegex;
 
 
